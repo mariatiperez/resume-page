fa5f1d4ce9bd4e66cefaebad5f4e23e118f06576
Maria Tirado <mariatiperez@gmail.com>
2020-04-25 20:30:46 -0400

    Updates

---
+++ precache-manifest.52d4681bd002e6713164163ddd2b7380.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "835fd20557df15283474ed8416b478f5",
+    "url": "/resume-page/index.html"
+  },
+  {
+    "revision": "939ae5cf5806470fe5a7",
+    "url": "/resume-page/static/css/2.043b003d.chunk.css"
+  },
+  {
+    "revision": "cbe2ac40e3bd6ac0a9a5",
+    "url": "/resume-page/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "939ae5cf5806470fe5a7",
+    "url": "/resume-page/static/js/2.6186e9e1.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/resume-page/static/js/2.6186e9e1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cbe2ac40e3bd6ac0a9a5",
+    "url": "/resume-page/static/js/main.bbb4bdda.chunk.js"
+  },
+  {
+    "revision": "0a49418505461a6594a0",
+    "url": "/resume-page/static/js/runtime-main.90c42a2e.js"
+  },
+  {
+    "revision": "53da16c5e9726a00ae12600601acbd6e",
+    "url": "/resume-page/static/media/github.53da16c5.svg"
+  },
+  {
+    "revision": "86cc47b613057859dfff4372f1dd8cb7",
+    "url": "/resume-page/static/media/gmail.86cc47b6.svg"
+  },
+  {
+    "revision": "d849bc2557228a905cb46a21f1819d90",
+    "url": "/resume-page/static/media/linkedin.d849bc25.svg"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/resume-page/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "6e7e69151e23dfb36aef3fe25d675b08",
+    "url": "/resume-page/static/media/menu.6e7e6915.svg"
+  },
+  {
+    "revision": "2e4cc66b686cceb90edbbef81bce27d0",
+    "url": "/resume-page/static/media/stack-overflow.2e4cc66b.svg"
+  }
+]);
